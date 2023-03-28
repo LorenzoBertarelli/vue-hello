0bc2ddfbc8133f47bcb6d66b208b2f2ed0af5a06
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-03-28 22:06:47 +0200

    esercizio e bonus

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const {createApp} = Vue;
+
+createApp({
+    data() {
+    return {
+        message: "Hello Vue",
+        img: `https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png`
+    }
+    }
+}).mount ("#app");
